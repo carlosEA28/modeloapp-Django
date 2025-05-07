4708986a8fe2abf1db96e6e9504f64be7e8ea41b
Erick Marchioro <erickmarchioro.dev@gmail.com>
2025-05-07 10:21:52 -0300

    Criacao da socialNetwork

---
+++ apps/SocialNetworks/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SocialnetworksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'SocialNetworks'
+    verbose_name = "SocialNetwork"
