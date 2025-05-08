07eb11c3df16c19ebf94cb80b4546cc299189552
Erick Marchioro <erickmarchioro.dev@gmail.com>
2025-05-07 21:14:19 -0300

    produto totalmente finalizado

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -5,4 +5,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('adicionar/', views.add_products, name='add_products'),
+    path("", views.list_products, name="list_products"),
+    path("editar/<int:id_product>/", views.edit_products, name="edit_products"),
+    path("excluir/<int:id_product>/", views.delete_products, name="delete_products"),
 ]
--- apps/product/views.py
+++ apps/product/views.py
@@ -17,3 +17,30 @@ def add_products(request):
     form = ProductForm()
     context['form'] = form
     return render(request, template_name, context)
+
+def list_products(request):
+    template_name = "product/list_products.html"
+    products = Product.objects.filter()  
+    context = {"products": products}
+    return render(request, template_name, context)
+
+def edit_products(request, id_product):
+    template_name = "product/add_products.html"
+    context = {}
+    product = get_object_or_404(Product, id=id_product)
+
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect("products:list_products")
+    redirect("products:list_products")
+    form = ProductForm(instance=product)
+    context["form"] = form
+    return render(request, template_name, context)
+
+
+def delete_products(request, id_product):
+    product = Product.objects.get(id=id_product)
+    product.delete()
+    return redirect("products:list_products")
